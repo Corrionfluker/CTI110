597c3ed8efcdf0a910a0a89e63afe8660d3d55d3
Corrionfluker <160172758+Corrionfluker@users.noreply.github.com>
2024-02-27 14:17:11 -0500

    Add files via upload

---
+++ P2LAB1_flukercory.py
@@ -0,0 +1,23 @@
+# Corrion Fluker
+# 2/27/24
+# P2LAB1
+# Formatting
+import math 
+radius=float(input("What is the radius of the circle?"))
+print()
+diameter=2*radius
+print(f'The diamater of the circle is',diameter)
+##print('The diameter of the circle is ',2*radius)
+circumfrence=2*3.14*radius
+print(f'The circumfrence of the circle is ',circumference)
+#print('The circumference of the circle is ',2*3.14*radius)
+area=3.14*math.pow(radius,2)
+#area=3.14*radius*radius
+print(f' The area of circle is {area:,3f}')
+print('The area of the circle is',format(area,'.3f'))
+
+      
+      
+
+      
+
